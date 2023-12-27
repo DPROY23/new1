31c5ec5fd83b5c4e326ccef78db97cfeb10c3c81
Vishal Golhar <135511723+mrvishalg2004@users.noreply.github.com>
2023-12-27 15:14:24 +0530

    Hambarger icon was responsive

--- script.js
+++ script.js
@@ -131,6 +131,14 @@ $(document).ready(function() {
       }
     });
   }
+  document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.querySelector('.menu_icon');
+    const navbar = document.querySelector('.navbar');
+
+    menuIcon.addEventListener('click', function () {
+      navbar.classList.toggle('active');
+    });
+  });
   
 
  
